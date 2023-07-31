2e4fe79f1ada3f6065b793349c632be808aed137
medosf <ostora.dev@gmail.com>
2023-07-31 07:17:56 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const cron = require('node-cron');
 const moment = require('moment-timezone');
-const fs = require('fs');
-const scrape = require('./scrape');
 
-// Schedule the job every minute, then check the time within the task
-cron.schedule('0 0 * * 0', async () => {
+cron.schedule('* * * * *', async () => {
     const currentTime = moment().tz('America/New_York');
         console.log('app running at', currentTime)
         console.log('Running  script');
-        const jobs = await scrape();
-        console.log(jobs); 
-
-        // Define a filename with the current date
-        const date = new Date();
-        const filename = `jobs-${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}.txt`;
-
-        // Save the job data to a text file
-        fs.writeFile(filename, JSON.stringify(jobs, null, 2), (err) => {
-            if (err) {
-                console.error('Error writing file', err);
-            } else {
-                console.log(`Saved job data to ${filename}`);
-            }
-        });
 });
