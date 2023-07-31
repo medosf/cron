aadf6fc7e6d572909f1818913d64c044a69cd8ec
medosf <ostora.dev@gmail.com>
2023-07-31 19:09:36 -0400

    adding sms service

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const cron = require('node-cron');
 const moment = require('moment-timezone');
+const {sendSms} = require('./smsService')
 
 cron.schedule('* * * * *', async () => {
     const currentTime = moment().tz('America/New_York');
         console.log('app running at', currentTime)
         console.log('Running  script');
+        sendSms()
 });
---
+++ smsService.js
@@ -0,0 +1,22 @@
+const twilio = require('twilio');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const sendSms = async(sendToNumber)=> {
+    const client = new twilio(accountSid, authToken);
+    const date = new Date()
+    try {
+       const res =  await  client.messages.create({
+            body: `Hello from Node at ${date}`,
+            to: sendToNumber || '+14159880574',  // Replace with your phone number
+            from: '+18663119127'
+        })
+        console.log('res',res)
+    }catch (err){
+        console.log('err',err)
+    }
+}
+    
+
+module.exports = {sendSms}
